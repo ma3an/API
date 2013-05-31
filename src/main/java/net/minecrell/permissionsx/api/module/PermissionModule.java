e3760ad9707998a83444e19e91e6e3eff60dbb8e
Minecrell <minecrell@gmail.com>
2013-05-31 22:41:00 +0200

    Update PermissionModule

--- src/main/java/net/minecrell/permissionsx/api/module/PermissionModule.java
+++ src/main/java/net/minecrell/permissionsx/api/module/PermissionModule.java
@@ -1,8 +1,11 @@
 package net.minecrell.permissionsx.api.module;
 
+import java.io.File;
+import java.io.InputStream;
 import java.util.logging.Logger;
 
 import org.bukkit.Server;
+import org.bukkit.plugin.Plugin;
 
 import net.minecrell.permissionsx.api.PermissionManager;
 
@@ -10,8 +13,23 @@ public interface PermissionModule {
 	
 	// TODO: Javadocs
 	
-	public abstract PermissionManager getManager();
+	public abstract String getName();
+	
 	public abstract Logger getLogger();
+	
+	
+	public abstract PermissionManager getManager();
+	public abstract Plugin getPluginContainer();
+	
 	public abstract Server getServer();
-
+	
+	
+	public abstract File getDataFolder();
+	
+	public abstract InputStream getResource(String fileName);
+	
+	public abstract void saveResource(String resourcePath);
+	public abstract void saveResource(String resourcePath, boolean replace);
+	
+	
 }
