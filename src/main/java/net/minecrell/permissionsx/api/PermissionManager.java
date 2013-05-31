0f99e78ebeb1a841fdb598049739b315ab55aef2
Minecrell <minecrell@gmail.com>
2013-05-31 23:08:52 +0200

    Remove abstract keyword from interface methods; add getServer to PermissionManager

--- src/main/java/net/minecrell/permissionsx/api/PermissionManager.java
+++ src/main/java/net/minecrell/permissionsx/api/PermissionManager.java
@@ -23,6 +23,7 @@ import net.minecrell.permissionsx.api.entity.OfflinePermissionUser;
 import net.minecrell.permissionsx.api.entity.PermissionGroup;
 import net.minecrell.permissionsx.api.entity.PermissionUser;
 
+import org.bukkit.Server;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
@@ -39,38 +40,41 @@ public interface PermissionManager {
 	 * Returns the current Logger with is used to Log Messages
 	 * @return the current Logger
 	 */
-	public abstract Logger getLogger();
+	public Logger getLogger();
 	
 	/**
 	 * Returns the Name of the PermissionsX implementation 
 	 * @return by default: <code>PermissionsX</code>
 	 */
-	public abstract String getName();
+	public String getName();
 	
 	/**
 	 * Returns the Version of the PermissionsX implementation
 	 * @return the Version by default in the Format: <br>
 	 * <code>{major version}.{minor version}.{patch version}</code>
 	 */
-	public abstract String getVersion();
+	public String getVersion();
 	
 	/**
 	 * Returns the Plugin the Permission Manager is integrated to.
 	 * @return the Plugin instance
 	 */
-	public abstract Plugin getContainer();
+	public Plugin getContainer();
+	
+	// TODO: Javadocs
+	public Server getServer();
 	
 	/**
 	 * Enables the Permission Manager
 	 * @throws java.lang.IllegalStateException if the Plugin already enabled.
 	 */
-	public abstract void enable() throws IllegalStateException ;
+	public void enable() throws IllegalStateException ;
 	
 	/**
 	 * Disables the Permission Manager
 	 * @throws IllegalStateException
 	 */
-	public abstract void disable() throws IllegalStateException;
+	public void disable() throws IllegalStateException;
 	
 	/**
 	 * Reload the Manager (e.g. Configuration Files)
@@ -85,7 +89,7 @@ public interface PermissionManager {
 	 * @return the User, may be null if the user can't be found or is not online
 	 * @see net.minecrell.permissionsx.api.entity.PermissionUser
 	 */
-	public abstract PermissionUser getUser(String name);
+	public PermissionUser getUser(String name);
 	
 	/**
 	 * Returns a Specific User identified by his username.
@@ -93,7 +97,7 @@ public interface PermissionManager {
 	 * @return the User, may be null if the user can't be found
 	 * @see net.minecrell.permissionsx.api.entity.PermissionUser
 	 */
-	public abstract OfflinePermissionUser getOfflineUser(String name);
+	public OfflinePermissionUser getOfflineUser(String name);
 	
 	/**
 	 * Returns a Specific Player identified by his username.
@@ -101,25 +105,25 @@ public interface PermissionManager {
 	 * @return the User, may be null if the user can't be found
 	 * @see org.bukkit.entity.Player
 	 */
-	public abstract Player getPlayer(String name);
+	public Player getPlayer(String name);
 	
 	/**
 	 * Returns a Specific Group identified by it's Group name
 	 * @param name the Group name
 	 * @return the Group instance. null if the group couldnt be found.
 	 */
-	public abstract PermissionGroup getGroup(String name);
+	public PermissionGroup getGroup(String name);
 	
 	/**
 	 * Returns the Configuration File used to load the Preferences from.
 	 * @return the Config file instance never null
 	 */
-	public abstract FileConfiguration getConfiguration();
+	public FileConfiguration getConfiguration();
 	
 	/**
 	 * Saves everything to the Disk. 
 	 * @throws java.lang.RuntimeException if a IOException occurs.
 	 */
-	public abstract void saveAll();
+	public void saveAll();
 	
 }
